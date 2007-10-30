ef0d8f60cd59d2175a800abbfc4c626c95961559
james <james@a0c33c4f-3f3c-0410-8ba4-d3650ac1a399>
2007-10-30 15:28:37 +0000

    refactoring the urligence test to make use of Shoulda for more flexibility
    git-svn-id: http://svn.jamesgolick.com/resource_controller/trunk@84 a0c33c4f-3f3c-0410-8ba4-d3650ac1a399

--- test/test/unit/urligence_test.rb
+++ test/test/unit/urligence_test.rb
@@ -12,69 +12,129 @@ class UrligenceTest < Test::Unit::TestCase
     @photo = stub(:class => stub(:name => "Photo"), :to_param => 1)
   end
   
-  def test_should_return_url_for_just_one_object_passed_to_it
-    setup_mocks "/photos/#{@photo.to_param}", :photo_path, @photo
-    assert_equal @expected_return, @controller.smart_url(@photo)
+  context "with one object" do
+    setup do
+      setup_mocks "/photos/#{@photo.to_param}", :photo_path, @photo
+    end
+
+    should "return the correct url" do
+      assert_equal @expected_return, @controller.smart_url(@photo)
+    end
   end
   
-  def test_should_return_url_for_two_objects_passed_to_it
-    setup_mocks "/tags/#{@tag.to_param}/photos/#{@photo.to_param}", :tag_photo_path, @tag, @photo
-    assert_equal @expected_return, @controller.smart_url(@tag, @photo)
+  context "with two objects" do
+    setup do
+      setup_mocks "/tags/#{@tag.to_param}/photos/#{@photo.to_param}", :tag_photo_path, @tag, @photo
+    end
+
+    should "return the correct url" do
+      assert_equal @expected_return, @controller.smart_url(@tag, @photo)
+    end
   end
   
-  def test_should_accept_namespace_as_first_param
-    setup_mocks "/admin/tags/#{@tag.to_param}/photos/#{@photo.to_param}", :admin_tag_photo_path, @tag, @photo
-    assert_equal @expected_return, @controller.smart_url(:admin, @tag, @photo)
+  context "with a namespace as first param" do
+    setup do
+      setup_mocks "/admin/tags/#{@tag.to_param}/photos/#{@photo.to_param}", :admin_tag_photo_path, @tag, @photo
+    end
+    
+    should "return the correct url" do
+      assert_equal @expected_return, @controller.smart_url(:admin, @tag, @photo)
+    end
   end
   
-  def test_should_accept_nil_options_anywhere
-    setup_mocks "/tags/#{@tag.to_param}/photos/#{@photo.to_param}", :tag_photo_path, @tag, @photo
-    assert_equal @expected_return, @controller.smart_url(nil, nil, nil, @tag, nil, @photo, nil)
+  context "with many nil options anywhere in the arguments" do
+    setup do
+      setup_mocks "/tags/#{@tag.to_param}/photos/#{@photo.to_param}", :tag_photo_path, @tag, @photo
+    end
+    
+    should "return the correct url" do
+      assert_equal @expected_return, @controller.smart_url(nil, nil, nil, @tag, nil, @photo, nil)
+    end
   end
   
-  def test_should_interpret_a_plural_symbol_as_the_last_parameter_to_mean_that_we_want_the_plural_path
-    setup_mocks "/tags/#{@tag.to_param}/photos", :tag_photos_path, @tag
-    assert_equal @expected_return, @controller.smart_url(@tag, :photos)
+  context "with a symbol as the last parameter" do
+    setup do
+      setup_mocks "/tags/#{@tag.to_param}/photos", :tag_photos_path, @tag
+    end
+
+    should "use that as the last fragment of the url" do
+      assert_equal @expected_return, @controller.smart_url(@tag, :photos)
+    end
   end
   
-  def test_should_take_a_plural_symbol_as_the_only_parameter
-    setup_mocks "/photos", :photos_path
-    assert_equal @expected_return, @controller.smart_url(nil, :photos)
+  context "with a symbol as the only parameter" do
+    setup do
+      setup_mocks "/photos", :photos_path
+    end
+
+    should "use that as the only url fragment" do
+      assert_equal @expected_return, @controller.smart_url(nil, :photos)
+    end
   end
   
-  def test_should_accept_a_namespace_with_a_plural
-    setup_mocks "/admin/products", :admin_products_path
-    assert_equal @expected_return, @controller.smart_url(:admin, :products)
+  context "with a namespace, and a plural symbol" do
+    setup do
+      setup_mocks "/admin/products", :admin_products_path
+    end
+
+    should "call the correct url handler" do
+      assert_equal @expected_return, @controller.smart_url(:admin, :products)
+    end
   end
   
-  def test_should_accept_only_symbols
-    setup_mocks '/admin/products/new', :new_admin_products_path
-    assert_equal @expected_return, @controller.smart_url(:new, :admin, :products)
+  context "with only symbols" do
+    setup do
+      setup_mocks '/admin/products/new', :new_admin_products_path
+    end
+    
+    should "return the correct url" do
+      assert_equal @expected_return, @controller.smart_url(:new, :admin, :products)
+    end
   end
   
-  def test_should_accept_array_parameters_for_routes_with_alternate_names
-    setup_mocks '/something_tags/1', :something_tag_path, @tag
-    assert_equal @expected_return, @controller.smart_url([:something_tag, @tag])
+  context "with array parameters for specifying the names of routes that don't match the class name of the object" do
+    setup do
+      setup_mocks '/something_tags/1', :something_tag_path, @tag
+    end
+
+    should "use the name of the symbol as the url fragment" do
+      assert_equal @expected_return, @controller.smart_url([:something_tag, @tag])
+    end
   end
   
-  def test_should_accept_array_parameters_with_symbol_parameters
-    setup_mocks '/admin/something_tags/1', :admin_something_tag_path, @tag
-    assert_equal @expected_return, @controller.smart_url(:admin, [:something_tag, @tag])
+  context "with array parameters and a namespace" do
+    setup do
+      setup_mocks '/admin/something_tags/1', :admin_something_tag_path, @tag
+    end
+
+    should "return the correct url" do
+      assert_equal @expected_return, @controller.smart_url(:admin, [:something_tag, @tag])
+    end
   end
   
-  def test_should_accept_array_parameters_with_symbol_namespace_and_symbol_ending_parameters
-    setup_mocks '/admin/something_tags/1/photos', :admin_something_tag_photos_path, @tag
-    assert_equal @expected_return, @controller.smart_url(:admin, [:something_tag, @tag], :photos)
+  context "with array parameters, a namespace, and an ending symbol" do
+    setup do
+      setup_mocks '/admin/something_tags/1/photos', :admin_something_tag_photos_path, @tag
+    end
+
+    should "return the correct url" do
+      assert_equal @expected_return, @controller.smart_url(:admin, [:something_tag, @tag], :photos)
+    end
   end
   
-  def test_should_accept_array_parameters_with_symbol_namespace_and_normal_model_parameters
-    setup_mocks '/admin/something_tags/1/photos/1', :admin_something_tag_photo_path, @tag, @photo
-    assert_equal @expected_return, @controller.smart_url(:admin, [:something_tag, @tag], @photo)
+  context "with array parameters, a symbol namespace, and normal model parameters" do
+    setup do
+      setup_mocks '/admin/something_tags/1/photos/1', :admin_something_tag_photo_path, @tag, @photo
+    end
+
+    should "return the correct url" do
+      assert_equal @expected_return, @controller.smart_url(:admin, [:something_tag, @tag], @photo)
+    end
   end
   
   private
     def setup_mocks(expected_return, method, *params)
       @expected_return = expected_return
-      @controller.expects(method).with(*params).returns(@expected_return)
+      @controller.stubs(method).with(*params).returns(@expected_return)
     end
 end
