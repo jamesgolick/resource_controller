0c74d93962052e247e4e5a97c970f278d271b897
james <james@a0c33c4f-3f3c-0410-8ba4-d3650ac1a399>
2007-11-10 14:47:42 +0000

    refactor controller module
    git-svn-id: http://svn.jamesgolick.com/resource_controller/trunk@95 a0c33c4f-3f3c-0410-8ba4-d3650ac1a399

--- lib/resource_controller/controller.rb
+++ lib/resource_controller/controller.rb
@@ -12,49 +12,50 @@ module ResourceController
         cattr_accessor :action_options
         self.action_options ||= {}
 
-        (ResourceController::ACTIONS - ResourceController::FAILABLE_ACTIONS).each do |action|
-          class_scoping_reader action, ResourceController::ActionOptions.new
+        ACTIONS.each do |action|
+          class_scoping_reader action, FAILABLE_ACTIONS.include?(action) ? FailableActionOptions.new : ActionOptions.new
           self.action_options[action] = send action
-        end
-
-        ResourceController::FAILABLE_ACTIONS.each do |action|
-          class_scoping_reader action, ResourceController::FailableActionOptions.new
-          self.action_options[action] = send action
-        end
-
-        index.wants.html
-        edit.wants.html
-        new_action.wants.html
-
-        show do
-          wants.html
-
-          failure.wants.html { render :text => "Member object not found." }
-        end
-
-        create do
-          flash "Successfully created!"
-          wants.html { redirect_to object_url }
-
-          failure.wants.html { render :action => "new" }
-        end
-
-        update do
-          flash "Successfully updated!"
-          wants.html { redirect_to object_url }
-
-          failure.wants.html { render :action => "edit" }
-        end
-
-        destroy do
-          flash "Successfully removed!"
-          wants.html { redirect_to collection_url }
         end
 
         self.helper_method :object_url, :edit_object_url, :new_object_url, :collection_url, :object, :collection, 
                              :parent, :parent_type, :parent_object, :model_name, :model
       end
+      
+      init_default_actions(subclass)
     end
         
+    private
+      def self.init_default_actions(klass)
+        klass.class_eval do
+          index.wants.html
+          edit.wants.html
+          new_action.wants.html
+
+          show do
+            wants.html
+
+            failure.wants.html { render :text => "Member object not found." }
+          end
+
+          create do
+            flash "Successfully created!"
+            wants.html { redirect_to object_url }
+
+            failure.wants.html { render :action => "new" }
+          end
+
+          update do
+            flash "Successfully updated!"
+            wants.html { redirect_to object_url }
+
+            failure.wants.html { render :action => "edit" }
+          end
+
+          destroy do
+            flash "Successfully removed!"
+            wants.html { redirect_to collection_url }
+          end
+        end
+      end
   end
 end
