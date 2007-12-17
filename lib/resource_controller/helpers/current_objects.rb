01ccfca18d5382fd9238b76239f7f0e7e556c5d8
james <james@a0c33c4f-3f3c-0410-8ba4-d3650ac1a399>
2007-12-17 17:01:01 +0000

    load parents
    git-svn-id: http://svn.jamesgolick.com/resource_controller/branches/deep_nesting@132 a0c33c4f-3f3c-0410-8ba4-d3650ac1a399

--- lib/resource_controller/helpers/current_objects.rb
+++ lib/resource_controller/helpers/current_objects.rb
@@ -43,15 +43,21 @@ module ResourceController::Helpers::CurrentObjects
     # Used internally to load the member object in to an instance variable @#{model_name} (i.e. @post)
     #
     def load_object
-      instance_variable_set "@#{parent_type}", parent_object if parent?
+      load_parents      
       instance_variable_set "@#{object_name}", object
     end
     
     # Used internally to load the collection in to an instance variable @#{model_name.pluralize} (i.e. @posts)
     #
     def load_collection
-      instance_variable_set "@#{parent_type}", parent_object if parent?
+      load_parents
       instance_variable_set "@#{object_name.to_s.pluralize}", collection
+    end
+    
+    def load_parents
+      if parent?
+        parent_objects.each { |parent_type, parent_object| instance_variable_set "@#{parent_type}", parent_object }
+      end
     end
   
     # Returns the form params.  Defaults to params[model_name] (i.e. params["post"])
