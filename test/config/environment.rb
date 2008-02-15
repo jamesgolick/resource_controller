d26dd6ac58748965d435229f77d4174a8e6bad9b
james <james@a0c33c4f-3f3c-0410-8ba4-d3650ac1a399>
2008-02-15 20:02:20 +0000

    try merging back to master
    git-svn-id: http://svn.jamesgolick.com/resource_controller/trunk@142 a0c33c4f-3f3c-0410-8ba4-d3650ac1a399

--- test/config/environment.rb
+++ test/config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '1.2.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.0.2' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -42,6 +42,8 @@ Rails::Initializer.run do |config|
   # config.active_record.default_timezone = :utc
   
   # See Rails::Configuration for more options
+  
+  config.action_controller.session = { :session_key => "_myapp_session", :secret => "some secret phrase" }
 end
 
 require "#{RAILS_ROOT}/../init"
