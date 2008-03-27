cda4d00b4a129e0d91463570bb1a24cb911a8d6b
james <james@a0c33c4f-3f3c-0410-8ba4-d3650ac1a399>
2008-03-27 19:40:17 +0000

    use inheritable attributes for class_reader_writer
    git-svn-id: http://svn.jamesgolick.com/resource_controller/trunk@163 a0c33c4f-3f3c-0410-8ba4-d3650ac1a399

--- lib/resource_controller/accessors.rb
+++ lib/resource_controller/accessors.rb
@@ -52,26 +52,22 @@ module ResourceController # :nodoc:
       def class_reader_writer(*accessor_names)
         accessor_names.each do |accessor_name|
           class_eval <<-"end_eval", __FILE__, __LINE__
-            unless defined? @@#{accessor_name}
-              @@#{accessor_name} = nil
-            end
-        
             def self.#{accessor_name}(*args)
               unless args.empty?
-                @@#{accessor_name} = args.first if args.length == 1
-                @@#{accessor_name} = args if args.length > 1
+                write_inheritable_attribute :#{accessor_name}, args.first if args.length == 1
+                write_inheritable_attribute :#{accessor_name}, args if args.length > 1
               end
             
-              @@#{accessor_name}
+              read_inheritable_attribute :#{accessor_name}
             end
           
             def #{accessor_name}(*args)
               unless args.empty?
-                @@#{accessor_name} = args.first if args.length == 1
-                @@#{accessor_name} = args if args.length > 1
+                self.class.write_inheritable_attribute :#{accessor_name}, args.first if args.length == 1
+                self.class.write_inheritable_attribute :#{accessor_name}, args if args.length > 1
               end
             
-              @@#{accessor_name}
+              self.class.read_inheritable_attribute :#{accessor_name}
             end
           end_eval
         end
