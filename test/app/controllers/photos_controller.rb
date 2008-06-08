e0245776224c0c4aea0cd31fa66aabee3005c540
James Golick <james@james-mbp.local>
2008-06-08 10:52:05 -0400

    make it possible for flash to accept a block, so that you can interpolate strings, etc

--- test/app/controllers/photos_controller.rb
+++ test/app/controllers/photos_controller.rb
@@ -2,6 +2,7 @@ class PhotosController < ResourceController::Base
   actions :all, :except => :update
   
   belongs_to :user
+  create.flash { "#{@photo.title} was created!" }
   
   private
     def parent_model
