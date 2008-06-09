9f4b231c8d7e944841ecd5c548d29c71b7845812
“grin” <grinkiewicz@gmail.com>
2008-06-09 20:10:08 +0200

    resource_controller with singleton support

--- lib/resource_controller.rb
+++ lib/resource_controller.rb
@@ -4,8 +4,11 @@ module ResourceController
   NAME_ACCESSORS   = [:model_name, :route_name, :object_name]  
   
   module ActionControllerExtension
-    def resource_controller
+    def resource_controller(*args)
       include ResourceController::Controller
-    end
+      if args.first == :singleton
+        include ResourceController::Helpers::SingletonCustomizations
+      end      
+    end  
   end
 end
